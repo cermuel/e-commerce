55e3bfce89380e9aacf3e5aaf3cd6e8a90ae9676
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2023-03-27 11:22:00 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex w-screen h-screen">
+      <div className="w-[40%] max-md:w-full h-full bg-pry-color"></div>
+      <div className="w-[60%] max-md:hidden h-full bg-[white]"></div>
+    </div>
+  );
+}
